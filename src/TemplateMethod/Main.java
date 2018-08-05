3ac68022f60ce441ae14d0e11c410f0c4aeb05b2
Godfavorstheinfantry <994523036@qq.com>
2018-08-05 23:31:47 +0800

    模板方法

--- src/TemplateMethod/Main.java
+++ src/TemplateMethod/Main.java
@@ -4,7 +4,11 @@ package TemplateMethod;
  * @Description 模板方法模式
  */ 
 public class Main {
-	 
+	/**
+	 * 模板方法模式的优点是可以在抽象父类中编写算法
+	 * 要求父类子类之间的协作与一致性
+	 * 处理的流程被定义在父类中，而具体的处理则交给了子类
+	 */ 
 	public static void main(String[] args) {
 		AbstractDisplay d1=new CharDisplay('H');
 		AbstractDisplay d2=new StringDisplay("aaaaaaaaaa");
